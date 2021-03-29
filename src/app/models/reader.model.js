15d978e79c277c2698e47128b19c43f3ddd5e1a8
TRUNG LÊ VĂN <18120622@student.hcmus.edu.vn>
2021-03-29 18:54:38 +0700

    fix avt post

--- src/app/models/reader.model.js
+++ src/app/models/reader.model.js
@@ -21,8 +21,7 @@ module.exports = {
 
     loadAllPosts(page, limit) {
         return ReaderModel.find()
-            .populate({ path: 'user', select: 'username' })
-            .populate({ path: 'user', select: 'avt' })
+            .populate({ path: 'user', select: ['avt', 'username'] })
             .populate('book')
             .sort({ date: -1 })
             .skip((page - 1) * limit)
